fe2da2b791c3afd1a6e4630439d00350e2aa5dc1
nowoszko <30315346+nowoszko@users.noreply.github.com>
2017-07-20 14:21:46 +0200

    a

--- webroot/scripts/php/db.php
+++ webroot/scripts/php/db.php
@@ -1,9 +1,9 @@
 <?php 
 
 define('ADRES',''.$_SERVER['MYSQL_HOST'].'');
-define('UZYTKOWNIK','lancuchswiatla');
+define('UZYTKOWNIK','scianaswiatla');
 define('HASLO_DO_BAZY',''.$_SERVER['MYSQL_PASS'].'');
-define('BAZA','lancuchswiatla'); 
+define('BAZA','scianaswiatla'); 
 
 $b2za=mysqli_connect(ADRES,UZYTKOWNIK,HASLO_DO_BAZY,BAZA);
 mysqli_query($b2za,"SET NAMES utf8;");
