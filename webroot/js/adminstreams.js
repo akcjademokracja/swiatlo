7e91d7dc415ff75f268002e70c1372e30ac81686
nowoszko <30315346+nowoszko@users.noreply.github.com>
2017-07-22 15:06:51 +0200

    a

--- webroot/js/adminstreams.js
+++ webroot/js/adminstreams.js
@@ -165,24 +165,7 @@ function showVideo(id, name) {
  				},
 				success: function(dane1){ 
 					$("#video").html(dane1);
-					 var my_video_player;
-(function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-js.src = "https://connect.facebook.net/pl_PL/all.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-     window.fbAsyncInit = function() {
-
-     FB.init({
-      appId      : '104628140213092',
-      xfbml      : true,
-      version    : 'v2.10'
-    });
- FB.Event.subscribe('xfbml.ready', function(msg) {
-	 
-	 });}
+					
  
   				 }} );   
     
--- webroot/scripts/php/showVideo.php
+++ webroot/scripts/php/showVideo.php
@@ -10,6 +10,23 @@ $stream=mysqli_fetch_array(mysqli_query($b2za, "SELECT * FROM  streams   where i
 		?>
  
 <script>   
-		 
+		  var my_video_player;
+(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+js.src = "https://connect.facebook.net/pl_PL/all.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+     window.fbAsyncInit = function() {
+
+     FB.init({
+      appId      : '104628140213092',
+      xfbml      : true,
+      version    : 'v2.10'
+    });
+ FB.Event.subscribe('xfbml.ready', function(msg) {
+	 
+	 });}
 	 </script>
 	 	<div class="fb-video" data-href="<?php echo $stream['address']; ?>" data-show-text="false" data-autoplay="false"></div>
