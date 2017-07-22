482f3ec4477390e4ed47567742236d3e9441f906
nowoszko <30315346+nowoszko@users.noreply.github.com>
2017-07-22 14:42:41 +0200

    a

--- webroot/scripts/php/brokenLink.php
+++ webroot/scripts/php/brokenLink.php
@@ -1,15 +1,12 @@
 <?php
-	ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ 
 
 	include('db.php');
 	include('dx.php'); 
 
 
 $update=mysqli_query($b2za, "UPDATE  streams set broken=1 where id='".$_POST['id']."'");
-echo mysqli_error($b2za);
- echo json_decode(array('ok'=>'ok'));
+  echo json_encode(array('ok'=>'ok'));
 			 
 		
 		
